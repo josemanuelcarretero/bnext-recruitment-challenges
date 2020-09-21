2505430c8a800210f9a1d3fac480fd8c428b2d18
Jose Manuel Carretero <josemanuelcarreterocuenca@gmail.com>
2020-09-21 17:44:54 +0200

    Add test to catch invalid routes

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,16 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { HttpModule, INestApplication } from '@nestjs/common';
 import { AppModule } from '../src/app.module';
+import { NotFoundExceptionFilter } from '../src/common/filters/not-found-exception.filter';
+import * as request from 'supertest';
 
 describe('AppController (e2e)', () => {
     let app: INestApplication;
+    let server;
 
-    beforeEach(async () => {
+    beforeAll(async () => {
         const moduleFixture: TestingModule = await Test.createTestingModule({
-            imports: [AppModule]
+            imports: [AppModule, HttpModule]
         }).compile();
 
         app = moduleFixture.createNestApplication();
+        app.useGlobalFilters(new NotFoundExceptionFilter());
+
         await app.init();
+        server = app.getHttpServer();
+    });
+
+    afterAll(async () => {
+        await server.close();
+        await server.disconnect();
+        await app.close().then(() => {
+            console.log('close');
+        });
+    });
+
+    it('throws error if URL is invalid', async done => {
+        const response = await request(server)
+            .get('/')
+            .expect(404);
+
+        expect(response.body.error).toEqual('invalid_url');
+        expect(response.body.statusCode).toEqual(404);
+        done();
     });
 });
